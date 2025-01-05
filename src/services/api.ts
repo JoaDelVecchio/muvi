17a3894e9c8258dcd8df5966e46a40fb1b95b27a
Joaquin Del Vecchio <172492638+JoaDelVecchio@users.noreply.github.com>
2025-01-05 12:30:41 -0300

    Add env variables

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
-const API_KEY = "a55b9a114646c17b022fded10f514813";
-const BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_API_KEY;
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const getPopularMovies = async () => {
   const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
